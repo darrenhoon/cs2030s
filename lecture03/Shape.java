eb5990235272594041e421aec68c72e6a32358b5
Darren <darrenhoon.ad@gmail.com>
2020-09-04 08:41:27 +0800

    updated lecture03 items

--- lecture03/Shape.java
+++ lecture03/Shape.java
@@ -1,35 +1,39 @@
 interface Scalable{
-    Scalable scaleBy(double factor){}
+	Scalable scaleBy(double factor);
 }
 
 abstract class Shape {
-    abstract double getArea();
-    abstract double getPerimeter();
-
-    public String toString(){
-        return "area" + this.area + " perimeter" + this.perimeter;
-    }
+	abstract double getArea();
+	abstract double getPerimeter();
 }
 
 class Rectangle extends Shape implements Scalable{
-    Rectangle(double length, double breadth){
-        this.length = length;
-        this.breadth = breadth;
-    }
-    @Override
-    public Rectangle scaleBy(double factor){
-        return new Rectangle(this.length*factor, this.breadth*factor);
-    }
+	private final double length;
+	private final double breadth;
+	Rectangle(double length, double breadth){
+		this.length = length;
+		this.breadth = breadth;
+	}
+	
+	public String toString(){
+		String message = String.format("Area is: %.2f, Perimeter is: %.2f",this.getArea(),this.getPerimeter();
+		return message;
+	}
 
-    @Override
-    public double getArea(){
-        double value = this.length * this.breadth;
-        return value;
-    }
+	@Override
+	public Rectangle scaleBy(double factor){
+		return new Rectangle(this.length*factor, this.breadth*factor);
+	}
 
-    @Override
-    public double getPerimeter(){
-        double value = this.length*2 + this.breadth*2;
-        return value;
-    }
+	@Override
+	public double getArea(){
+		double value = this.length * this.breadth;
+		return value;
+	}
+
+	@Override
+	public double getPerimeter(){
+		double value = this.length*2 + this.breadth*2;
+		return value;
+	}
 }
--- recit02/recitation2.java
+++ recit02/recitation2.java
@@ -1,63 +1,78 @@
 public class Point{
-    private final double x;
-    private final double y;
+	private final double x;
+	private final double y;
 
-    public Point(double x, double y){
-        this.x=x;
-        this.y=y;
-    }
+	public Point(double x, double y){
+		this.x=x;
+		this.y=y;
+	}
 }
 
 public class Circle{
-    private final Point centre;
-    private final int radius;
+	final Point centre;
+	final int radius;
 
-    public Circle(Point p, int radius){
-        this.centre =p;
-        this.radius = radius;
-    }
+	public Circle(Point p, int radius){
+		this.centre =p;
+		this.radius = radius;
+	}
 
-    @Override
-    public boolean equals(Object obj){
-        System.out.println("equals(Object) called");
-        if(obj==this){return true;}
-        if(obj instanceof Circle){
-            Circle circle = (Circle) obj;
-            return (circle.centre.equals(centre) && circle.radius==radius);
-        }
-        else{return false;}
-    }
-    public boolean equals(Circle circle){
-        System.out.println("equals(Circle) called");
-        return (circle.centre.equals(this.centre) && circle.radius==this.radius);
-    }
+	@Override
+	public boolean equals(Object obj){
+		System.out.println("equals(Object) called! ");
+		if(obj==this){
+			System.out.println("Same object!");
+			return true;
+		}
+		if(obj instanceof Circle){
+			System.out.println("object is an instance of Circle called!");
+			Circle circle = (Circle) obj;
+			return (circle.centre.equals(centre) && circle.radius==radius);
+		}
+		else{
+			System.out.println("input parameter is not an object or is not equal at all! >:(");
+			return false;
+		}
+	}
+	public boolean equals(Circle circle){
+		System.out.println("equals(Circle) called");
+		return (circle.centre.equals(this.centre) && circle.radius==this.radius);
+	}
 }
 
 public class Rectangle{
-    private double height;
-    private double base;
-    private double area;
-    private double perimeter;
+	private double height;
+	private double base;
+	private double area;
+	private double perimeter;
 
-    public Rectangle(double height,double base){
-        this.height=height;
-        this.base=base;
-        this.area = height*base;
-        this.perimeter = 2*height + 2*base;
-    }
+	public Rectangle(double height,double base){
+		this.height=height;
+		this.base=base;
+		this.area = height*base;
+		this.perimeter = 2*height + 2*base;
+	}
 
-    public String toString(){
-        String message = String.format("area %.2f and perimeter %.2f",this.area,this.perimeter);
-        return message;
-    }
+	public String toString(){
+		String message = String.format("height is: %.2f and width is: %.2f",this.height,this.base);
+		return message;
+	}
+
+	public Rectangle scaleBy(double factor){
+		return new Rectangle(this.height*factor,this.base*factor);
+	}
+
+	public Rectangle setHeight(double height){
+		return new Square(height);
+	}
 }
 
 class Square extends Rectangle{
-    Square(double length){
-        super(length,length);
-    }
+	Square(double length){
+		super(length,length);
+	}
 
-    public Square setHeight(double height){
-        return new Square(height);
-    }
+	public Square setHeight(double height){
+		return new Square(height);
+	}
 }
