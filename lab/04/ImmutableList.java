b7b40411b1dfd61fdc213183309655f1c33f9b2a
Darren <darrenhoon.ad@gmail.com>
2020-09-20 11:51:19 +0800

    rearranged checkstyle location, edited lab/04

--- lab/04/ImmutableList.java
+++ lab/04/ImmutableList.java
@@ -1,17 +1,17 @@
 import java.util.List;
-import java.util.Arrays;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.lang.IllegalArgumentException;
 import java.lang.NullPointerException;
-
+import java.util.function.Predicate;
+import java.util.function.Function;
 
 class ImmutableList<T> {
     private final List<T> list;
 
     @SafeVarargs
-    ImmutableList(T ... args) {        
+    ImmutableList(T... args) {        
         List<T> tempList = new ArrayList<T>(Arrays.asList(args));
         this.list = tempList;
     }
@@ -25,16 +25,16 @@ class ImmutableList<T> {
     @Override
     public String toString() {
         String message = "[";
-        int i =0;
-        if(this.getLength() == 0) {
+        int i = 0;
+        if (this.getLength() == 0) {
             return "[]";
         }
 
-        while(i<this.getLength()-1) {
+        while (i < this.getLength() - 1) {
             message += this.getItem(i) + ", ";
             i++;
         }
-        message += this.getItem(i) +"]";
+        message += this.getItem(i) + "]";
         return message;
     }
 
@@ -48,8 +48,8 @@ class ImmutableList<T> {
     ImmutableList<T> remove(T item) {
         List<T> tempList = new ArrayList<T>();
         boolean flag = true;
-        for(T i: this.list) {
-            if(i == item && flag) {
+        for (T i: this.list) {
+            if (i == item && flag) {
                 flag = false;
                 continue;
             } else {
@@ -59,11 +59,11 @@ class ImmutableList<T> {
         return new ImmutableList<T>(tempList);
     }
 
-    ImmutableList<T> replace(T item, T new_item) {
+    ImmutableList<T> replace(T item, T newItem) {
         List<T> tempList = new ArrayList<T>();
         for (T i: this.list) {
-            if(i == item) {
-                tempList.add(new_item);
+            if (i == item) {
+                tempList.add(newItem);
             } else {
                 tempList.add(i);
             }
@@ -71,9 +71,28 @@ class ImmutableList<T> {
         return new ImmutableList<T>(tempList);
     }
 
+    ImmutableList<T> filter(Predicate<? super T> pred) {
+        List<T> tempList = new ArrayList<T>();
+        for (T elem: this.list) {
+            if (pred.test(elem)) {
+                tempList.add(elem);
+            }
+        }
+        return new ImmutableList<T>(tempList);
+    }
+
+    <R> ImmutableList<R> map(Function<? super T,? extends R> func) {
+        List<R> tempList = new ArrayList<R>();
+        for (T elem: this.list) {
+            R mappedValue = (R) func.apply((T) elem);
+            tempList.add(mappedValue);
+        }
+        return new ImmutableList<R>(tempList);
+    }
+ 
     int getLength() {
         int i =  0;
-        for(T item: this.list) {
+        for (T item: this.list) {
             i++;
         }
         return i;
@@ -85,7 +104,7 @@ class ImmutableList<T> {
             throw new IllegalArgumentException("limit size < 0");
         }
         long i = 0;
-        while(i<this.getLength() && i<length) {
+        while (i < this.getLength() && i < length) {
             tempList.add(this.list.get((int) i));
             i++;
         }
@@ -99,17 +118,16 @@ class ImmutableList<T> {
     boolean equals(ImmutableList<T> list) {
         if (list == this) {
             return true;
-        }
-        else {
+        } else {
             int i = 0;
             int listLength = list.getLength();
             int thisLength = this.getLength();
-            if(listLength != thisLength) {
+            if (listLength != thisLength) {
                 return false;
             }
 
-            while(i<listLength) {
-                if (this.getItem(i) != list.getItem(i) ) {
+            while (i < listLength) {
+                if (this.getItem(i) != list.getItem(i)) {
                     return false;
                 }
                 i++;
@@ -119,7 +137,7 @@ class ImmutableList<T> {
     }
 
     ImmutableList<T> sorted(Comparator<T> comp) {
-        if(comp == null) {
+        if (comp == null) {
             throw new NullPointerException("Comparator is null");
         }
 
@@ -127,12 +145,12 @@ class ImmutableList<T> {
         List<T> referenceList = new ArrayList<T>();
         referenceList.addAll(this.list);
 
-        while(referenceList.size() != 0) {
+        while (referenceList.size() != 0) {
             T ref = referenceList.get(0);
 
-            for(int j=0;j<referenceList.size();j++) {
+            for (int j = 0; j < referenceList.size(); j++) {
                 T current = referenceList.get(j);
-                if(comp.compare(current,ref) < 0) {
+                if (comp.compare(current,ref) < 0) {
                     ref = current;
                 }
             }
@@ -148,14 +166,14 @@ class ImmutableList<T> {
         return tempList.toArray();
     }
 
-    <T extends Object> T[] toArray(T arr[]) {
-        if(arr == null) {
+    <T extends Object> T[] toArray(T[] arr) {
+        if (arr == null) {
             throw new NullPointerException("Input array cannot be null");
         }
         try {
             T[] tempArray = this.list.toArray(arr);
             return tempArray;
-        } catch(ArrayStoreException e) {
+        } catch (ArrayStoreException e) {
             throw new ArrayStoreException("Cannot add element to array as it is the wrong type");
         }
     }
