862d65a4237572cb1613e9eada7869860ab1449a
Darren <darrenhoon.ad@gmail.com>
2020-09-30 11:03:14 +0800

    still trying to package items

---
+++ project_package/Event.java
@@ -0,0 +1,12 @@
+package cs2030.simulator;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+abstract class Event {
+    abstract Event execute();
+    abstract double getServiceTime();
+    abstract Customer getCustomer();
+    abstract List<Server> getServerList();
+    abstract double getCurrentTime();
+}
---
+++ project_package/tt.jsh
@@ -0,0 +1,8 @@
+/open Event.java
+/open Customer.java
+/open Server.java
+/open ArriveEvent.java
+/open ServeEvent.java
+/open DoneEvent.java
+/open LeaveEvent.java
+/open WaitEvent.java
