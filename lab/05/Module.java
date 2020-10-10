63cd3c0d532cf1f2fb1df2d520f1fb8d1ac36977
Darren <darrenhoon.ad@gmail.com>
2020-10-10 18:00:25 +0800

    added lab 05

---
+++ lab/05/Module.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class Module extends KeyableMap {
+
+    Module(String mod) {
+        super(mod);
+    }
+}
+
+/*
+    for (Map.Entry<Key,Value> entry : set.entrySet()) {
+        entry.getKey()
+        entry.getValue()
+    }
+*/
---
+++ lab/05/Student.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class Student implements KeyableMap {
+
+    Student(String name) {
+        super(name);
+    }
+
+}
