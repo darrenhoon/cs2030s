097979a258ff2810c2f6a0f02b092aae35d11853
Darren <darrenhoon.ad@gmail.com>
2020-10-26 11:18:12 +0800

    added recit09 and recit08 corrections

--- lab/07/Lazy.java
+++ lab/07/Lazy.java
@@ -2,8 +2,11 @@ import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.function.Supplier;
 import java.util.function.Function;
+import java.util.function.BiFunction;
+import java.util.function.Predicate;
+import java.lang.Comparable;
 
-class Lazy<T> {
+class Lazy<T extends Comparable> {
 
     private Optional<Supplier<T>> supplier;
     private Optional<T> value;
@@ -47,5 +50,30 @@ class Lazy<T> {
         } catch (NoSuchElementException e) {
             return "?";
         }
+    }
+
+    public <R> Lazy<R> map(Function<? super T,? extends R> func) {
+        return Lazy.of(() -> func.apply(this.get()));
+    }
+
+    public <R> Lazy<R> flatMap(Function<? super T, ? extends Lazy<R>> func){
+        return Lazy.of(() -> func.apply(this.get()).get());
+    }
+
+    public <R,U> Lazy<R> combine(Lazy<U> lazy, BiFunction<T,U,R> func) {
+        return Lazy.of(() -> func.apply(this.get(), lazy.get()));
+    }
+
+    public Lazy<Boolean> test(Predicate<T> pred) {
+        return Lazy.of(pred.test(this.get()));
+    }
+
+    public boolean equals(Object o) {
+        if ((o instanceof Lazy) == false) {
+            return false;
+        }
+        Lazy<?> lazy = (Lazy<?>) o;
+        boolean result = (this.get() == lazy.get());
+        return result;
     }
 }
